e8e905a9e77d109fab90656b619362f8efe64699
Matthieu Lauze <matthieu.lauze@protonmail.com>
2025-12-05 17:13:25 +0100

    feat : load input day 1 from text file directly in the main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
+fn load_input() -> Result<String, String> {
+    let file_path = "src/day1/input.txt";
+    println!("Input file {file_path}");
+
+    if let Ok(contents) = fs::read_to_string(file_path) {
+        Ok(contents)
+    } else {
+        return Err("Should have been able to read the file".into());
+    }
+}
+
+fn day1() {
+    let Ok(input) = load_input() else {
+        panic!("load input failed for day 1.")
+    };
+    // let mut dial = 50;
+    let vec_input: Vec<&str> = input.split('\n').collect();
+    println!("rotations count {}", vec_input.len());
+}
+
 fn main() {
-    println!("Hello, world!");
+    day1();
 }
